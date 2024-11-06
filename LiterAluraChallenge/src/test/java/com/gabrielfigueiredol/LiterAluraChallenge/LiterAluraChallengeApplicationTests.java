19aa947a94edba7575c84ff43666ec9e39866a88
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-11-06 14:25:05 -0300

    first commit

---
+++ LiterAluraChallenge/src/test/java/com/gabrielfigueiredol/LiterAluraChallenge/LiterAluraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielfigueiredol.LiterAluraChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
